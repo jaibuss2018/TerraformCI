883fcc0a3aeddfa7323c60585333cb15bf9287b7
jaibuss2018 <jaibuss2018@yahoo.com>
2025-05-14 13:50:28 +0000

    added

--- main.tf
+++ main.tf
@@ -41,7 +41,7 @@ resource "azurerm_storage_account" "storageaccount" {
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
-resource "azurerm_storage_account" "storageaccount" {
+resource "azurerm_storage_account" "storageaccount1" {
   name                     = "stg${random_string.uniquestring.result}"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
